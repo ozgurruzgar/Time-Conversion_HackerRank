e6eaf833e0dbdace664cba7417f8ad20d153a304
Özgür <zgr@DESKTOP-1NLBACI>
2023-01-26 16:45:17 +0300

    Add project files.

---
+++ Time-Conversion_HackerRank/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace Time_Conversion_HackerRank
+{
+
+    class Result
+    {
+
+        /*
+         * Complete the 'timeConversion' function below.
+         *
+         * The function is expected to return a STRING.
+         * The function accepts STRING s as parameter.
+         */
+
+        public static string timeConversion(string s)
+        {
+            var dt = DateTime.ParseExact(s,"hh:mm:ss:tt",CultureInfo.InvariantCulture);
+
+            return $"{dt:HH:mm:ss}";
+        }
+
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string s = Console.ReadLine();
+
+            string result = Result.timeConversion(s);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+
+}
